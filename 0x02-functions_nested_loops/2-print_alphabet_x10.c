24a8fcfc3deea7aa1b2ce20075ef9f88fa1dbdcf
GcinileM <gci19.mag@gmail.com>
2023-02-21 10:45:01 -0800

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - make alphabet x10 times
+ * print_alphabet_x10 - make alphabet x10 times, in lowercase
  */
 
 void print_alphabet_x10(void)
 {
+
 	int i = 0;
 	char c;
 
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
-        {
+	{
 	_putchar(c);
-	}
+
 	_putchar('\n');
 	i++;
-	}
+	{
 }
